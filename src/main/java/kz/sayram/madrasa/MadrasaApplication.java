71b73a1cd7dcfe8ea56f700c8194420c4d08a619
bilolbek <bilolbek00@gmail.com>
2022-04-03 17:27:45 +0600

    default project - generated on spring initialazer with: PostgreSQL Driver, Spring Data JPA, Flyway Migration, Spring Web, Lombok

---
+++ src/main/java/kz/sayram/madrasa/MadrasaApplication.java
@@ -0,0 +1,13 @@
+package kz.sayram.madrasa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MadrasaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MadrasaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/sayram/madrasa/MadrasaApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.sayram.madrasa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MadrasaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
